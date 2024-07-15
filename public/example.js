5372f0de251aab792bb164e171d24f4d0d469ab7
Ian Love <122408357+IanNathanielLove@users.noreply.github.com>
2024-07-15 15:33:10 -0600

    maths

--- public/example.js
+++ public/example.js
@@ -11,3 +11,39 @@ function alertMessageChange() {
     const b = 3;
     console.log(a + b);
 }
+
+
+var x = 3
+console.log(x)
+
+const name = "Ian"
+console.log(name)
+
+var output = x + name
+console.log(output)
+
+var y = 5
+
+var output2 = x + y 
+console.log(output2)
+
+if (y > x) {
+    console.log("X is greater than Y")
+}
+
+
+for (let i = 0; i < y; i++) {
+    console.log(i)
+}
+
+while (x > 0) {
+    console.log(x)
+    x--
+}
+
+
+function add(a, b) {
+    return a + b
+}
+
+console.log(add(3, 5))
