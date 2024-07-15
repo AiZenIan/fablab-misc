2c5ece3515bcfa083303a5c0edabc31d4dfa3117
Ian Love <122408357+IanNathanielLove@users.noreply.github.com>
2024-07-15 14:53:14 -0600

    test-vercek

---
+++ public/example.js
@@ -0,0 +1,13 @@
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     document.getElementById('alert-button').addEventListener("click", function() {
+//         alert('Hello, world!');
+//     });
+// });
+
+
+function alertMessageChange() {
+    alert('Hello, world!');
+    const a = 5;
+    const b = 3;
+    console.log(a + b);
+}
